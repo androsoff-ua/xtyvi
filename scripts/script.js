49bc1d50c54da8ff3a7d37b2f60479e12d6bd111
androsoff-ua <androsoff@hotmail.com>
2022-10-04 20:11:57 +0300

    Add JS
    Add basic js

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,7 @@
-let text1 = document.querySelector('.primer');
-console.log(text1);
+let textTitle = document.querySelector('.card-title');
+let textField = document.querySelector('.text');
+
+textTitle.innerHTML = 'Тестовий текст';
+textField.innerHTML = '<strong>Тестовий</strong> текст завдання, який буде тут знаходитись.';
+
+console.log(textField.innerHTML);
