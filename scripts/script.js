022bf9004618ad3f3fb389d463f9bbb809566114
androsoff-ua <androsoff@hotmail.com>
2022-10-11 09:55:25 +0300

    Add Jquery update page

--- scripts/script.js
+++ scripts/script.js
@@ -4,42 +4,50 @@ function getRandomInRange(min, max) {
 
 import cards from "../json/card1.json" assert {type: 'json'};
 
-async function elementUpdate(selector) {
-  try {
-    var html = await (await fetch(location.href)).text();
-    var newdoc = new DOMParser().parseFromString(html, 'text/html');
-    document.querySelector(selector).outerHTML = newdoc.querySelector(selector).outerHTML;
-    console.log('Элемент '+selector+' был успешно обновлен');
-    return true;
-  } catch(err) {
-    console.log('При обновлении элемента '+selector+' произошла ошибка:');
-    console.dir(err);
-    return false;
-  }
-}
-
-function newCard (cards) {
-let title = document.querySelector('.card-title');
-let textField = document.querySelector('.text');
-let i = getRandomInRange (0,4);
-//let i = 3;
-let img = document.createElement('img');
-img.src = cards.cards[i].img;
-
-textField.innerHTML = cards.cards[i].text;
-textField.appendChild(img);
-title.innerHTML = cards.cards[i].title;
-
-}
+let num = smartRandom(0,7);
+let index =0;
 
 function smartRandom(min,max) {
-let num=[];
-for (let i=min; i<=max; i++){
-num[i] = getRandomInRange(min,max);
-console.log(num[i]);
+  let num=[];
+  for (let i=min; i<=max; i++){
+    while (num.length != max - min) {
+      let value = getRandomInRange(min, max);
+      if (!num.includes(value)) {
+        num.push(value);
+      }
+    }
+  }
+  console.log(num);
+  return num;
+  }
 
-}
+function showCard (cards,num, i) {
+let title = document.querySelector('.card-title');
+let textField = document.querySelector('.text');
+let img = document.createElement('img');
+img.src = cards.cards[num[i]].img;
+
+textField.innerHTML = cards.cards[num[i]].text;
+textField.appendChild(img);
+title.innerHTML = cards.cards[num[i]].title;
 }
 
-smartRandom(0,2);
-newCard(cards);
+
+
+$(document).ready(function(){
+$('#next').click(function(){
+  if(index<=5){
+      index++;
+      showCard(cards,num,index);
+      console.log("Index ["+index+"] = "+num[index]);
+  }
+  else{
+    console.log("Все!");
+  }
+
+})
+})
+
+
+
+showCard(cards,num, index);
